d02371b3afc07792d1ba3c1d52acf8232907a406
Veerapavani <2211cs010433@mallareddyuniversity.ac.in>
2025-01-10 13:19:05 +0530

    Add files via upload

---
+++ fun-declaration.js
@@ -0,0 +1,25 @@
+function sum(a , b)
+{
+   let c = a + b 
+   return c
+   console.log("hello")
+}
+console.log(sum(10+10));
+console.log(sum(100 , 100));
+
+function operation():
+{
+   let a = Number(window.prompt("enter a value"))
+   let a = Number(window.prompt("enter b value"))
+   let c = a + b
+   let d = a - b
+   let e = a * b;
+   console.log(c)
+   console.log(d);
+   console.log(e);
+
+}
+
+
+
+
